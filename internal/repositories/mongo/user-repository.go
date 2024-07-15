e941abed7f60d38dc61f0fbd67cfb27017f844a4
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-07-15 16:52:46 +0200

    feat: check if user already exists

--- internal/repositories/mongo/user-repository.go
+++ internal/repositories/mongo/user-repository.go
@@ -2,8 +2,10 @@ package repositories
 
 import (
 	"context"
+	"errors"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -11,6 +13,10 @@ import (
 const (
 	DATABASE_NAME   = "faceit"
 	COLLECTION_NAME = "users"
+)
+
+var (
+	ErrUserAlreadyExist = errors.New("the user already exist in the db")
 )
 
 type UserRepository struct {
@@ -24,14 +30,29 @@ func NewUserRepository(client *mongo.Client) *UserRepository {
 func (u *UserRepository) AddUser(ctx context.Context, user *User) error {
 	log.Println("Adding a user to the database")
 
-	err := addHashedPassword(user)
+	if err := userAlreadyExists(ctx, u.collection, user.Nickname, user.Email); err != nil {
+		return err
+	}
+
+	if err := addHashedPassword(user); err != nil {
+		return err
+	}
+
+	if _, err := u.collection.InsertOne(ctx, user); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func userAlreadyExists(ctx context.Context, collection *mongo.Collection, nickname, email string) error {
+	count, err := collection.CountDocuments(ctx, bson.M{"nickname": nickname, "email": email})
 	if err != nil {
 		return err
 	}
 
-	_, err = u.collection.InsertOne(ctx, user)
-	if err != nil {
-		return err
+	if count > 0 {
+		return ErrUserAlreadyExist
 	}
 
 	return nil
--- internal/repositories/mongo/user-repository_test.go
+++ internal/repositories/mongo/user-repository_test.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"context"
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,63 +13,102 @@ import (
 )
 
 func TestRepository(t *testing.T) {
-	t.Run("Add a new user", func(t *testing.T) {
-		ctx := context.Background()
+	t.Run("AddUser", func(t *testing.T) {
+		t.Run("Add a new user", func(t *testing.T) {
+			ctx := context.Background()
 
-		mongodbContainer, err := mongodb.Run(ctx, "mongo:7")
-		if err != nil {
-			log.Fatalf("failed to start container: %s", err)
-		}
+			mongodbContainer, err := mongodb.Run(ctx, "mongo:7")
+			assert.NoError(t, err, "failed to terminate container: %s", err)
 
-		defer func() {
-			if err := mongodbContainer.Terminate(ctx); err != nil {
-				log.Fatalf("failed to terminate container: %s", err)
-			}
-		}()
+			defer func() {
+				err := mongodbContainer.Terminate(ctx)
+				assert.NoError(t, err, "failed to terminate container: %s", err)
+			}()
 
-		endpoint, err := mongodbContainer.ConnectionString(ctx)
-		if err != nil {
-			log.Fatalf("failed to get connection string: %s", err)
-		}
+			endpoint, err := mongodbContainer.ConnectionString(ctx)
+			assert.NoError(t, err, "failed to get connection string: %s", err)
 
-		mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(endpoint))
-		if err != nil {
-			log.Fatalf("failed to connect to MongoDB: %s", err)
-		}
+			mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(endpoint))
+			assert.NoError(t, err, "failed to connect to MongoDB: %s", err)
 
-		err = mongoClient.Ping(ctx, nil)
-		if err != nil {
-			log.Fatalf("failed to ping MongoDB: %s", err)
-		}
+			err = mongoClient.Ping(ctx, nil)
+			assert.NoError(t, err, "failed to ping MongoDB: %s", err)
 
-		userRepo := NewUserRepository(mongoClient)
-		userRepo.AddUser(ctx, &User{
-			FirstName: "testName",
-			LastName:  "testLastName",
-			Nickname:  "testNickname",
-			Email:     "testEmail@email.com",
-			Country:   "UK",
-			Password:  "testPassword",
+			userRepo := NewUserRepository(mongoClient)
+			userRepo.AddUser(ctx, &User{
+				FirstName: "testName",
+				LastName:  "testLastName",
+				Nickname:  "testNickname",
+				Email:     "testEmail@email.com",
+				Country:   "UK",
+				Password:  "testPassword",
+			})
+
+			result := mongoClient.
+				Database("faceit").
+				Collection("users").
+				FindOne(ctx, bson.M{"nickname": "testNickname", "email": "testEmail@email.com"})
+
+			assert.NoError(t, result.Err())
+			userResult := &User{}
+			err = result.Decode(userResult)
+			assert.NoError(t, err)
+			assert.NotNil(t, userResult)
+			assert.Equal(t, "testName", userResult.FirstName)
+			assert.Equal(t, "testLastName", userResult.LastName)
+			assert.Equal(t, "testNickname", userResult.Nickname)
+			assert.Equal(t, "testEmail@email.com", userResult.Email)
+			assert.Equal(t, "UK", userResult.Country)
+			assert.NotEmpty(t, userResult.Id)
+			assert.NotEmpty(t, userResult.Password)
+			assert.NoError(t, bcrypt.CompareHashAndPassword([]byte(userResult.Password), []byte("testPassword")))
 		})
 
-		result := mongoClient.
-			Database("faceit").
-			Collection("users").
-			FindOne(ctx, bson.M{"nickname": "testNickname", "email": "testEmail@email.com"})
+		t.Run("Return an error if the user already exist", func(t *testing.T) {
+			ctx := context.Background()
 
-		assert.NoError(t, result.Err())
-		userResult := &User{}
-		err = result.Decode(userResult)
-		assert.NoError(t, err)
-		assert.NotNil(t, userResult)
-		assert.Equal(t, "testName", userResult.FirstName)
-		assert.Equal(t, "testLastName", userResult.LastName)
-		assert.Equal(t, "testNickname", userResult.Nickname)
-		assert.Equal(t, "testEmail@email.com", userResult.Email)
-		assert.Equal(t, "UK", userResult.Country)
-		assert.NotEmpty(t, userResult.Id)
-		assert.NotEmpty(t, userResult.Password)
-		assert.NoError(t, bcrypt.CompareHashAndPassword([]byte(userResult.Password), []byte("testPassword")))
+			mongodbContainer, err := mongodb.Run(ctx, "mongo:7")
+			assert.NoError(t, err, "failed to terminate container: %s", err)
+
+			defer func() {
+				err := mongodbContainer.Terminate(ctx)
+				assert.NoError(t, err, "failed to terminate container: %s", err)
+			}()
+
+			endpoint, err := mongodbContainer.ConnectionString(ctx)
+			assert.NoError(t, err, "failed to get connection string: %s", err)
+
+			mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(endpoint))
+			assert.NoError(t, err, "failed to ping MongoDB: %s", err)
+
+			err = mongoClient.Ping(ctx, nil)
+			assert.NoError(t, err, "failed to ping MongoDB: %s", err)
+
+			_, err = mongoClient.
+				Database("faceit").
+				Collection("users").
+				InsertOne(ctx, &User{
+					Id:        "randomID",
+					FirstName: "testName",
+					LastName:  "testLastName",
+					Nickname:  "testNickname",
+					Email:     "testEmail@email.com",
+					Country:   "UK",
+					Password:  "testPwd",
+				})
+			assert.NoError(t, err)
+
+			userRepo := NewUserRepository(mongoClient)
+			err = userRepo.AddUser(ctx, &User{
+				FirstName: "testName",
+				LastName:  "testLastName",
+				Nickname:  "testNickname",
+				Email:     "testEmail@email.com",
+				Country:   "UK",
+				Password:  "testPassword",
+			})
+			assert.Error(t, err, ErrUserAlreadyExist.Error())
+		})
 	})
 
 	t.Run("Modify an existing user", func(t *testing.T) {
