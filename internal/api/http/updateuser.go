c969f5b4be7148f256e4c6b808ce5d7e9947726b
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-07-17 22:38:10 +0200

    feat: UpdateUserHandler

---
+++ internal/api/http/updateuser.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/dlion/faceit_challenge/internal/domain/services/user"
+	"github.com/gorilla/mux"
+)
+
+func (u *UserHandler) UpdateUserHandler(w http.ResponseWriter, req *http.Request) {
+	var updateUser user.UpdateUser
+	if err := json.NewDecoder(req.Body).Decode(&updateUser); err != nil {
+		log.Print(err)
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(req)
+	id, ok := vars["id"]
+	if !ok || id == "" {
+		log.Print("Update failed, it has been provided a bad ID")
+		http.Error(w, "ID parameter missing in URL", http.StatusBadRequest)
+		return
+	}
+	updateUser.Id = id
+
+	updatedUser, err := u.UserService.UpdateUser(req.Context(), updateUser)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(updatedUser); err != nil {
+		log.Print(err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+}
---
+++ internal/api/http/updateuser_test.go
@@ -0,0 +1,67 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/dlion/faceit_challenge/internal/domain/services/user"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateUserHandler(t *testing.T) {
+	updateUser := user.UpdateUser{
+		FirstName: "John",
+		LastName:  "Doe",
+		Nickname:  "johnd",
+		Email:     "john.doe@example.com",
+		Password:  "password123",
+		Country:   "USA",
+	}
+	jsonData, err := json.Marshal(updateUser)
+	if err != nil {
+		t.Fatalf("Failed to marshal update user: %v", err)
+	}
+
+	req, err := http.NewRequest("PUT", "/api/user/66981a71a4fd0f7ff33251b1", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	mockedUserService := new(MockUserService)
+	userHandler := UserHandler{UserService: mockedUserService}
+	mockedUserService.On("UpdateUser").Return(&user.User{
+		Id:        "66981a71a4fd0f7ff33251b1",
+		FirstName: "John",
+		LastName:  "Doe",
+		Nickname:  "johnd",
+		Email:     "john.doe@example.com",
+		Country:   "USA",
+		CreatedAt: time.Now().String(),
+		UpdatedAt: time.Now().String(),
+	})
+	router := mux.NewRouter()
+	router.HandleFunc("/api/user/{id}", userHandler.UpdateUserHandler).Methods("PUT")
+
+	router.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	var updatedUser user.User
+	err = json.NewDecoder(rr.Body).Decode(&updatedUser)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, updatedUser.Id)
+	assert.Equal(t, updateUser.FirstName, updatedUser.FirstName)
+	assert.Equal(t, updateUser.LastName, updatedUser.LastName)
+	assert.Equal(t, updateUser.Nickname, updatedUser.Nickname)
+	assert.Equal(t, updateUser.Email, updatedUser.Email)
+	assert.Equal(t, updateUser.Country, updatedUser.Country)
+	assert.NotEmpty(t, updatedUser.CreatedAt)
+	assert.NotEmpty(t, updatedUser.UpdatedAt)
+}
